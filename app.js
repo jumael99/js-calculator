765b08ef4fc1b9f1a175d430668e789d99ef73d1
jumael99 <jumael99@gmail.com>
2024-05-08 18:06:19 +0600

    added calc functionality

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
-body {
 
+let mainString = "";
+
+function addValueToString() {
+    mainString = eval(mainString);
+    show();
 }
+
+function show() {
+    document.querySelector(".show-result-screen").innerText = mainString;
+}
+
+document.querySelector('.five-part').addEventListener('click', function (event) {
+    let value = event.target;
+    if (value.tagName === "BUTTON") {
+        if (value.innerText !== 'Ac' && value.innerText !== 'Del' && value.innerText !== '=') {
+            mainString += value.innerText;
+            show();
+        }
+        if (value.innerText === 'Ac') {
+            mainString = "";
+            show();
+        }
+        if (value.innerText === 'Del') {
+            mainString = mainString.substring(0,mainString.length-1);
+            show();
+        }
+        if (value.innerText === '=') {
+            addValueToString();
+            show();
+        }
+    }
+})
+
