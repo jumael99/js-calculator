4b1361d1ce44ed0a7180f4d4293a73d309336fd8
jumael99 <jumael99@gmail.com>
2024-05-09 16:38:11 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+let mainString = "";
+
+function show() {
+    document.querySelector(".show-result-screen").innerText = mainString;
+}
+
+function showAndClean() {
+    document.querySelector(".show-result-screen").innerText = mainString;
+    mainString = "";
+}
+
+function addValueToString() {
+        mainString = String(eval(mainString));
+        showAndClean();
+}
+
+document.querySelector('.five-part').addEventListener('click', function(event) {
+    let value = event.target;
+    if (value.tagName === "BUTTON") {
+        const operators = ['*', '+', '-', '/'];
+        const lastChar = mainString[mainString.length - 1];
+
+        if (value.innerText === 'Ac') {
+            mainString = "";
+            show();
+        }
+        else if (value.innerText === 'Del') {
+            if (mainString === 'Infinity') {
+                mainString = "";
+            }
+            mainString = mainString.substring(0, mainString.length - 1);
+            show();
+        }
+        else if (value.innerText === '=') {
+            addValueToString();
+        }
+        else if (operators.includes(value.innerText) && operators.includes(lastChar)) {
+            show();
+        }
+        else {
+            mainString += value.innerText;
+            show();
+        }
+        console.log(mainString);
+    }
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+```javascript
+Welcome to Zumaels branch
+``` 
